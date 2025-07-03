6819d7a4e4f6de5d738402cfe0f9107c03405c2d
ViralMedia01 <viralmediaonline01@gmail.com>
2025-07-03 09:21:51 +0100

    supabaseClient.ts

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,6 +1,3 @@
-import { createPagesBrowserClient  } from '@supabase/auth-helpers-nextjs'
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
 
-export const supabase = createPagesBrowserClient (
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+export const supabase = createPagesBrowserClient()
